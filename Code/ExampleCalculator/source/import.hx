9727bf8ddd7bb9175006a8cb8f9e8f9fd4adce62
ChickenSwimmer2020 <105177647+ChickenSwimmer2020@users.noreply.github.com>
2025-02-26 10:15:30 -0500

    Update import.hx

--- Code/ExampleCalculator/source/import.hx
+++ Code/ExampleCalculator/source/import.hx
@@ -1,4 +1,11 @@
 package;
 
-import flixel.ui.*;
-import flixel.math.FlxMath;
+import openfl.display.Sprite;
+import flixel.FlxGame;
+import openfl.geom.Rectangle;
+import flixel.addons.ui.FlxUIAssets;
+import flixel.ui.FlxButton;
+import flixel.text.FlxText;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.FlxSubState;
